7d5c08f7ec1af7242bd7b306db23d724b26460b2
Deep Mallick <deepmallick2361@gmail.com>
2026-04-24 14:37:47 +0530

    Fix: Reload and logout error

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -2,8 +2,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export function PrivateRoute() {
-  const { user } = useAuth();
-
+  const { user, loading } = useAuth();
+  if(loading) {
+      return;
+  }
   if (!user) {
     return <Navigate to="/login" replace />;
   }
